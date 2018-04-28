e8833fefd621a76d9a9ab652cb5fa4e9d147476c
Chai <chai@fy.com>
2018-04-28 09:30:26 +0800

    积分商城v-1

--- work/js/lists.js
+++ work/js/lists.js
@@ -8,17 +8,17 @@ var userid = (getQueryString('userid')!=null ? getQueryString('userid') : null);
 $.cookie("userid",userid);
 
 var page=1;
-var pageSize=5;
+var pageSize=4;
 var loading = false;
 
 $(function () {
     $(".tabar ul li").on("click",function () {
-        $(this).addClass("curr").siblings().removeClass("curr");
+        $(this).stop().addClass("curr").siblings().stop().removeClass("curr");
         var type=$(this).attr("type");
-        console.info(type);
-        //切换加载
-        $(".goodList").html("");
         page=1;
+        //切换加载
+        loading=false;
+        $(".goodList").html("");
         getData(page,type);
     });
     //初始化加载
@@ -26,29 +26,21 @@ $(function () {
 
     //上拉加载
     $(document.body).infinite().on("infinite", function() {
+        console.log(loading);
         if(loading) return;
         loading = true;
         page=++page;
         setTimeout(function() {
             getData(page,$(".curr").attr("type"));
             loading = false;
-        }, 2000);
+        }, 300);
     });
 });
 
-//详情页
-function getDetail(goodsId) {
-    $.ajax({
-        type:'post',
-        url:'http://sge.cn:9090/erp/api/goodsInfo',
-        data:{"goodsid": goodsId},
-        success:function (res) {
-            console.info(res);
-        }
-    })
-}
-
 function getData(page,type) {
+    //进来时候加载
+    $(".weui-loadmore").html("<i class=\"weui-loading\"></i><span class=\"weui-loadmore__tips\">正在加载</span>");
+
     $.ajax({
         type:"POST",
         url:"http://sge.cn:9090/erp/api/storeHomePage",
@@ -61,6 +53,7 @@ function getData(page,type) {
         dataType:"json",
         success:function(res){
             if(res.code==200){
+                //console.info(res.data.goods.totalRow);
                 var str="";
                 if(res.data.goods.totalRow == 0){
                     console.info(res.data.order.totalRow);
@@ -73,36 +66,24 @@ function getData(page,type) {
                     $.each(res.data.goods.list,function (index,val){
                         str+="<li>" +
                             "<a class='clear block' href='detail.html?goodsId="+val.goodsId+"'>" +
-                                "<div class='picList fle'><img src='"+val.imageUrl+"?x-oss-process=image/resize,l_100'/></div>" +
-                                "<div class=\"picMargins\">" +
-                                    "<p class='font16'>"+val.goodsName+"</p>" +
-                                    "<p class='jf'><b class='font_red font18'>"+val.integral+"</b><span> 积分</span></p>" +
-                                "</div>" +
+                            "<div class='picList fle'><img src='"+val.imageUrl+"?x-oss-process=image/resize,l_100'/></div>" +
+                            "<div class=\"picMargins\">" +
+                            "<p class='font16'>"+val.goodsName+"</p>" +
+                            "<p class='jf'><b class='font_red font18'>"+val.integral+"</b><span> 积分</span></p>" +
+                            "</div>" +
                             "</a>" +
                             "</li>";
                     });
                     $('.goodList').append(str);
                     $(".weui-loadmore").show();
-                    //console.info(res.data.goods.totalPage);
-                    return;
                 }
-                if(res.data.goods.totalPage < page ){
-                    //console.info(page);
+
+                if(res.data.goods.totalPage == page ){
                     loading=true;
                     $(".weui-loadmore").html("<span class=\"weui-loadmore__tips\">我已经到底了...</span>");
                     return;
                 };
 
-
-                /*if(res.data.goods.lastPage==false){
-                    loading=false;
-                    $(".weui-loadmore").html("<i class=\"weui-loading\"></i>\n" +
-                        "       <span class=\"weui-loadmore__tips\">正在加载</span>");
-                }else{
-                    loading=true;
-                    $(".weui-loadmore").html(" <span class=\"weui-loadmore__tips\">我已经到底了...</span>");
-                    return;
-                };*/
             }else {
                 $(".weAlert").show();
             }
@@ -114,6 +95,17 @@ function getData(page,type) {
     });
 }
 
+//详情页
+function getDetail(goodsId) {
+    $.ajax({
+        type:'post',
+        url:'http://sge.cn:9090/erp/api/goodsInfo',
+        data:{"goodsid": goodsId},
+        success:function (res) {
+            console.info(res);
+        }
+    })
+}
 
 
 
